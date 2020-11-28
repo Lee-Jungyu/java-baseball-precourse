4483fb4924bfc768cbb9c1c1bc0ddc56138e11ea
jungyu09 <jungyu09@gmail.com>
2020-11-28 22:11:25 +0900

    feat: Implement input number reciever

--- src/main/java/baseball/GameMaker.java
+++ src/main/java/baseball/GameMaker.java
@@ -3,6 +3,7 @@ package baseball;
 import utils.RandomUtils;
 
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class GameMaker {
 
@@ -17,5 +18,31 @@ public class GameMaker {
         }
 
         return randomNumberArray;
+    }
+
+    public ArrayList<Integer> getInputNumberArray(Scanner scanner) {
+        ArrayList<Integer> inputNumberArray = new ArrayList<>();
+        System.out.print("숫자를 입력해주세요 : ");
+        String inputNumber = scanner.next();
+
+        //입력 값이 정수형이 맞는지 체크
+        for (char c : inputNumber.toCharArray()) {
+            if(!Character.isDigit(c)) throw new IllegalArgumentException();
+        }
+        //세 자리 수가 맞는지 체크
+        if(inputNumber.length() != 3) throw new IllegalArgumentException();
+        //입력 값에 0이 있는지 체크
+        if (inputNumber.contains("0")) throw new IllegalArgumentException();
+        //자리에 있는 값이 모두 다른지 체크
+        for(int i = 0; i < 2; i++) {
+            if(inputNumber.substring(i + 1, 3).contains(inputNumber.substring(i, i + 1)))
+                throw new IllegalArgumentException();
+        }
+
+        for(int i = 0; i < 3; i++) {
+            inputNumberArray.add(Integer.valueOf(inputNumber.charAt(i)));
+        }
+
+        return inputNumberArray;
     }
 }
