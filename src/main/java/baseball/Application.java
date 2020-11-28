f05a6b16c31a915109fbf388c6c7e49d83d973b5
jungyu09 <jungyu09@gmail.com>
2020-11-28 22:39:46 +0900

    feat: Implement Application class main method

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -6,5 +6,13 @@ public class Application {
     public static void main(String[] args) {
         final Scanner scanner = new Scanner(System.in);
         // TODO 구현 진행
+        int game = 1;
+        do {
+            GameMaker gameMaker = new GameMaker();
+            gameMaker.startGame(scanner);
+
+            System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+            game = scanner.nextInt();
+        } while(game == 1);
     }
 }
